37d5a5d0c53ce9a7b9fa9e0bb7531260d4640fb2
Ravikanth C <ravikanth@ravichaganti.com>
2021-10-29 22:19:16 +0530

    second policy

--- 01-Authoring/newGCPolicy.ps1
+++ 01-Authoring/newGCPolicy.ps1
@@ -2,11 +2,11 @@
 $guid = (New-Guid).guid
 
 New-GuestConfigurationPolicy `
--ContentUri "https://raw.githubusercontent.com/rchaganti/azconf2021/main/01-Authoring/virtualMemoryShouldBe50GB/virtualMemoryShouldBe50GB.zip" `
--DisplayName "Virtual memory should be set to at least 50GB" `
+-ContentUri "https://raw.githubusercontent.com/rchaganti/azconf2021/main/01-Authoring/TimezoneConfiguredAsDesired/TimezoneConfiguredAsDesired.zip" `
+-DisplayName "Timezone should be configured as desired" `
 -Platform 'Windows' `
--Description 'Ensure the VM virtual memory is configured to be at least 50GB in the C drive' `
+-Description 'Ensure that timezone is configured as specified' `
 -Mode ApplyAndMonitor `
 -PolicyId $guid `
--Path '.\virtualMemoryShouldBe50GB\Policies' `
+-Path '.\TimezoneConfiguredAsDesired\Policies' `
 -Verbose
--- 02-Publishing/publishGCPolicy.ps1
+++ 02-Publishing/publishGCPolicy.ps1
@@ -1,8 +1,17 @@
-# This is an optional step if you want to publish the package to storage account
+# 1.1 This is an optional step if you want to publish the package to storage account
 Publish-GuestConfigurationPackage -Path ./virtualMemoryShouldBe50GB/virtualMemoryShouldBe50GB.zip `
                 -ResourceGroupName azconf `
                 -StorageAccountName azconfchaganti `
                 -StorageContainerName policy | Select-Object -Property ContentUri
 
-# This publishes the policy definition to Azure Policy
+# 1.2 This publishes the policy definition to Azure Policy
 Publish-GuestConfigurationPolicy -Path ./virtualMemoryShouldBe50GB/Policies
+
+# 2.1 This is an optional step if you want to publish the package to storage account
+Publish-GuestConfigurationPackage -Path ./TimezoneConfiguredAsDesired/TimezoneConfiguredAsDesired.zip `
+                -ResourceGroupName azconf `
+                -StorageAccountName azconfchaganti `
+                -StorageContainerName policy | Select-Object -Property ContentUri
+
+# 2.2 This publishes the policy definition to Azure Policy
+Publish-GuestConfigurationPolicy -Path ./TimezoneConfiguredAsDesired/Policies
