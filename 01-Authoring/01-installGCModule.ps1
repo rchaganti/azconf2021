167da0b3ed92ba3caef2318d44fc7ecd8ad2fd15
Ravikanth C <ravikanth@ravichaganti.com>
2021-10-30 14:06:48 +0530

    Update 01-installGCModule.ps1

--- 01-Authoring/01-installGCModule.ps1
+++ 01-Authoring/01-installGCModule.ps1
@@ -1,5 +1,15 @@
-# Requires PowerShell 7.2 Preview
+# Requires PowerShell 7
 Install-Module -Name GuestConfiguration -Force
 
 # Get commands in Guest Configuration module
 Get-Command -Module GuestConfiguration
+
+# Install Guest Configuration Extension
+Set-AzVMExtension -Publisher 'Microsoft.GuestConfiguration' `
+        -Type 'ConfigurationforWindows' `
+        -Name 'AzurePolicyforWindows' `
+        -TypeHandlerVersion 1.0 `
+        -ResourceGroupName 'azconf' `
+        -Location 'eastus' `
+        -VMName 'azconfwin02' `
+        -EnableAutomaticUpgrade $true
