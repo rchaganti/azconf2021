b8c424aef14874c305704cb0ce22866736981297
Ravikanth C <ravikanth@ravichaganti.com>
2021-10-30 12:44:23 +0530

    Update newGCPolicy.ps1

--- 01-Authoring/newGCPolicy.ps1
+++ 01-Authoring/newGCPolicy.ps1
@@ -2,13 +2,13 @@
 $guid = (New-Guid).guid
 
 New-GuestConfigurationPolicy `
--ContentUri "https://raw.githubusercontent.com/rchaganti/azconf2021/main/01-Authoring/TimezoneConfiguredAsDesired/TimezoneConfiguredAsDesired.zip" `
--DisplayName "Timezone should be configured as desired" `
+-ContentUri "https://raw.githubusercontent.com/rchaganti/azconf2021/main/01-Authoring/virtualMemoryShouldBe50GB/virtualMemoryShouldBe50GB.mof"
+-DisplayName "Virtual memory should be set to at least 50GB" `
 -Platform 'Windows' `
--Description 'Ensure that timezone is configured as specified' `
+-Description 'Ensure the VM virtual memory is configured to be at least 50GB in the C drive' `
 -Mode ApplyAndMonitor `
 -PolicyId $guid `
--Path '.\TimezoneConfiguredAsDesired\Policies' `
+-Path '.\virtualMemoryShouldBe50GB\Policies' `
 -Verbose
 
 # 1.2 New Policy ID
