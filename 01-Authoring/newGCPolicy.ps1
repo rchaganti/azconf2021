78652eda01a00e25be11b81de38ad8953e6acdc1
Ravikanth C <ravikanth@ravichaganti.com>
2021-10-30 12:34:22 +0530

    updates

--- 01-Authoring/newGCPolicy.ps1
+++ 01-Authoring/newGCPolicy.ps1
@@ -1,4 +1,17 @@
-# New Policy ID
+# 1.1 New Policy ID
+$guid = (New-Guid).guid
+
+New-GuestConfigurationPolicy `
+-ContentUri "https://raw.githubusercontent.com/rchaganti/azconf2021/main/01-Authoring/TimezoneConfiguredAsDesired/TimezoneConfiguredAsDesired.zip" `
+-DisplayName "Timezone should be configured as desired" `
+-Platform 'Windows' `
+-Description 'Ensure that timezone is configured as specified' `
+-Mode ApplyAndMonitor `
+-PolicyId $guid `
+-Path '.\TimezoneConfiguredAsDesired\Policies' `
+-Verbose
+
+# 1.2 New Policy ID
 $guid = (New-Guid).guid
 
 New-GuestConfigurationPolicy `
